3a216e82f1db302e44ba721706c54b38863e23a0
dekinikis-tech <dekinikis@gmail.com>
2026-05-05 20:45:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ FILE_NAME   = "vps.txt"
 SUB_FILE    = "sub.txt"    # Base64-подписка для V2RayNG / Nekobox / Streisand
 VIEWER_FILE = "index.html"
 XRAY_BIN    = "xray"
-TOP_N_EACH  = 900   # топ отдельно для зарубежных И для российских
+TOP_N_EACH  = 2000   # топ отдельно для зарубежных И для российских
 
 # ============================================================
 # ФИЛЬТРЫ СЕРВЕРОВ
